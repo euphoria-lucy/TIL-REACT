abbb9575d9159c1bbc9b2748dea5e67c34dd0cce
misung <voielactee7@naver.com>
2024-02-11 23:25:10 +0900

    [React] 이벤트

--- code/src/App.js
+++ code/src/App.js
@@ -3,11 +3,18 @@ import "./App.css";
 
 // 함수 선언할 때 첫 문자는 무조건 대문자로!!
 function Header(props) {
-  console.log("props", props, props.title);
   return (
     <header>
       <h1>
-        <a href="/">{props.title}</a>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault(); // 기본 동작 방지
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
       </h1>
     </header>
   );
@@ -20,7 +27,16 @@ function Nav(props) {
     let t = props.topics[i];
     lis.push(
       <li key={t.id}>
-        <a href={"/read/" + t.id}>{t.title}</a>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
       </li>
     );
   }
@@ -49,8 +65,18 @@ function App() {
   ];
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header
+        title="WEB"
+        onChangeMode={() => {
+          alert("Header");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
