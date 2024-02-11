b64462bf8e5564d5f889ab12ad0fa8089a9e9672
misung <voielactee7@naver.com>
2024-02-11 21:32:24 +0900

    [React] 컴포넌트만들기

--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,52 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Header() {
+  // 함수 선언할 때 첫 문자는 무조건 대문자로!!
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+
 function App() {
-  return <div className="App">Hello React!</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
 }
 
 export default App;
